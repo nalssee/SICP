e58fc73897352a52ed1df895f51979f7a938cdc7
nalssee <kenjin@sdf.org>
2015-11-08 12:01:59 +0900

    ..

--- vseval/repl.py
+++ vseval/repl.py
@@ -13,33 +13,31 @@ from SICP.vseval.vseval import *
 
 PROMPT = "VSEVAL> "
 
-if __name__ == "__main__":
+print("""
+Vanilla Scheme interpreter for SICP Chapter 4
 
-    print("""
-    Vanilla Scheme interpreter for SICP Chapter 4
+After typing in a Lisp expression,
+press Enter followed by ctrl-d for evaluation.
 
-    After typing in a Lisp expression,
-    press Enter followed by ctrl-d for evaluation.
+(quit) or (exit) to finish this repl.
+""")
 
-    (quit) or (exit) to finish this repl.
-    """)
-
-    while True:
-        print(PROMPT, end='')
-        code_all = []
-        try:
-            while True:
-                try:
-                    code = input()
-                    code_all.append(code)
-                except EOFError:
-                    break
-            expr = parse(' '.join(code_all))
-            if expr == ['quit'] or expr == ['exit']:
-                print("Goodbye!!")
+while True:
+    print(PROMPT, end='')
+    code_all = []
+    try:
+        while True:
+            try:
+                code = input()
+                code_all.append(code)
+            except EOFError:
                 break
-            print("=> ", end='')
-            print(vseval(expr, GLOBAL_ENV))
+        expr = parse(' '.join(code_all))
+        if expr == ['quit'] or expr == ['exit']:
+            print("Goodbye!!")
+            break
+        print("=> ", end='')
+        print(vseval(expr, GLOBAL_ENV))
 
-        except Exception as e:
-            print(e)
+    except Exception as e:
+        print(e)
